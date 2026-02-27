17ed02d018bd3a6dc37ece9c2a7bea686765157b
Dronx <dronxx07@gmail.com>
2026-02-27 13:54:21 +0800

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -13,20 +13,20 @@ EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 
 def fetch_all_cookies():
-    with SB(headless=False, xvfb=True) as sb:
+    with SB(headless=False) as sb:
         sb.open(AMAZON_URL)
-        sb.sleep(10)
+        sb.sleep(30)
         amazon_cookie = sb.driver.get_cookie_string()
 
         sb.open(SELLER_URL)
-        sb.sleep(10)
+        sb.sleep(30)
         seller_cookie = sb.driver.get_cookie_string()
 
         sb.open(SAS_LOGIN_URL)
         sb.type("input[name='LoginForm[email]']", EMAIL)
         sb.type("input[name='LoginForm[password]']", PASSWORD)
         sb.click("button[type='submit']")
-        sb.sleep(15)
+        sb.sleep(30)
         sas_cookies = sb.driver.get_cookies()
 
         return amazon_cookie, seller_cookie, sas_cookies
