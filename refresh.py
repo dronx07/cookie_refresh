75ffcd56e59f7e24f0325f4640ac503a1d69ab40
Dronx <dronxx07@gmail.com>
2026-02-27 13:21:57 +0800

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -1,18 +1,22 @@
 import json
-import asyncio
-from seleniumbase import SB
-from playwright.async_api import async_playwright
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from seleniumbase import SB
 
 load_dotenv()
 
 SELLER_URL = "https://sellercentral-europe.amazon.com/"
 AMAZON_URL = "https://www.amazon.fr/"
+SAS_LOGIN_URL = "https://sas.selleramp.com/site/login"
 
 
 def get_cookie_string(sb):
     return sb.driver.get_cookie_string()
+
+
+def get_cookie_dict_list(sb):
+    """Return cookies as list of dicts (like Playwright did)."""
+    return sb.driver.get_cookies()
 
 
 def fetch_amazon_cookies():
@@ -28,43 +32,30 @@ def fetch_amazon_cookies():
         return seller_cookie, amazon_cookie
 
 
-class SASLogin:
-    def __init__(self, email: str, password: str, headless: bool = True):
-        self.login_url = "https://sas.selleramp.com/site/login"
-        self.email = email
-        self.password = password
-        self.headless = headless
+def fetch_sas_cookies(email, password, headless=False):
+    with SB(headless=headless, uc=True) as sb:
+        sb.open(SAS_LOGIN_URL)
 
-    async def login(self):
-        async with async_playwright() as p:
-            browser = await p.chromium.launch(
-                headless=self.headless,
-                args=["--disable-blink-features=AutomationControlled"]
-            )
-            context = await browser.new_context()
-            page = await context.new_page()
+        sb.type("input[name='LoginForm[email]']", email)
+        sb.type("input[name='LoginForm[password]']", password)
+        sb.click("button[type='submit']")
 
-            await page.goto(self.login_url, wait_until="load")
-            await page.fill("input[name='LoginForm[email]']", self.email)
-            await page.fill("input[name='LoginForm[password]']", self.password)
-            await page.click("button[type='submit']")
-            await page.wait_for_load_state("load")
-            await asyncio.sleep(30)
+        sb.sleep(30)
 
-            cookies = await context.cookies()
-            await browser.close()
+        sas_cookies = get_cookie_dict_list(sb)
 
-            return cookies
+        return sas_cookies
 
 
-async def main():
+def main():
     seller_cookie, amazon_cookie = fetch_amazon_cookies()
-    sas = SASLogin(
+
+    sas_cookies = fetch_sas_cookies(
         email=os.getenv("EMAIL"),
         password=os.getenv("PASSWORD"),
         headless=False
     )
-    sas_cookies = await sas.login()
+
     data = {
         "amazon": amazon_cookie,
         "seller_central": seller_cookie,
@@ -74,5 +65,6 @@ async def main():
     with open("cookies.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
